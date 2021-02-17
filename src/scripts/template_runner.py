eb741fa770eb1999103902d52cf31ea07c4460dc
hkir-dev <huseyin_kir@yahoo.com>
2021-02-17 17:10:04 +0300

    issue #31 Update ROBOT template generation scripts and build pipeline. Related sub tasks #34 #30 #32

--- src/scripts/template_runner.py
+++ src/scripts/template_runner.py
@@ -1,4 +1,5 @@
-from template_generation_tools import generate_curated_class_template, generate_ind_template, generate_marker_template
+from template_generation_tools import generate_curated_class_template, generate_ind_template, generate_marker_template, \
+    generate_equivalent_class_reification_template
 import argparse
 
 
@@ -12,12 +13,19 @@ parser.add_argument('-c', action='store_true',
 parser.add_argument('-m', action='store_true',
                     help="Generate a marker template.")
 
+parser.add_argument('-er', action='store_true',
+                    help="Generate a equivalent_class template with reification.")
+parser.add_argument('-em', action='store_true',
+                    help="Generate a equivalent_class template with marker.")
+
 args = parser.parse_args()
 
 if args.c:
     generate_curated_class_template(args.input, args.output)
 elif args.m:
     generate_marker_template(args.input, args.output)
+elif args.er:
+    generate_equivalent_class_reification_template(args.input, args.output)
 else:
     generate_ind_template(args.input, args.output)
 
