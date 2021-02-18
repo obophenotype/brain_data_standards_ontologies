004da35106ab286c6e5a2db290061bd1cf0614e7
hkir-dev <huseyin_kir@yahoo.com>
2021-02-18 18:35:59 +0300

    Add schema validation script #37 and update schema and the config content

--- src/config/config_schema_validator.py
+++ src/config/config_schema_validator.py
@@ -1,19 +1,15 @@
 import yaml
 import json
 import logging
-from jsonschema import validate
+import os
+from jsonschema import validate, ValidationError
 
 log = logging.getLogger(__name__)
 
 
-def read_taxonomy_details_yaml():
-    """
-    TODO: after merge of branch update_issue_31, use this function:
-    from template_generation_utils import read_taxonomy_details_yaml
-    Returns:
-
-    """
-    with open(r'../dendrograms/taxonomy_details.yaml') as file:
+def read_taxonomy_details_yaml(config_path):
+    absolute_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), config_path)
+    with open(absolute_path, 'r') as file:
         documents = yaml.full_load(file)
     return documents
 
@@ -27,12 +23,21 @@ def read_schema(schema_path):
     Returns: schema object
 
     """
-    f = open(schema_path, 'r')
+    absolute_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), schema_path)
+    f = open(absolute_path, 'r')
     return json.loads(f.read())
 
 
 if __name__ == '__main__':
-    validate(read_taxonomy_details_yaml(), read_schema("./config_schema.json"))
+    try:
+        log.info("Config schema validation started.")
+        validate(read_taxonomy_details_yaml('../dendrograms/taxonomy_details.yaml'),
+                 read_schema("./config_schema.json"))
+        log.info("Config schema validation successful.")
+    except ValidationError as e:
+        log.error("Config schema validation failed: " + e.message)
+        raise ValueError("Configuration schema validation failed: " + e.message)
+
 
 
 
