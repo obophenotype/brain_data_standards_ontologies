ab3fb739fe3d2259f68977e75a70e0a93930eb3f
hkir-dev <huseyin_kir@yahoo.com>
2021-03-08 20:58:51 +0300

    brainCellRegionMarker.tsv fixes #49

--- src/scripts/dosdp_pattern_generation.py
+++ src/scripts/dosdp_pattern_generation.py
@@ -1,25 +1,22 @@
 import pandas as pd
 import os
 import csv
-import networkx as nx
-import argparse
 
 from dendrogram_tools import dend_json_2_nodes_n_edges
-from template_generation_utils import read_taxonomy_details_yaml
+from template_generation_utils import read_taxonomy_config, get_subtrees
 from marker_tools import read_dendrogram_tree
 
 ALLEN_DEND_ = 'AllenDendClass:'
 
 
 def generate_pattern_table(dend_json_path, output_filepath):
-    config = read_taxonomy_details_yaml()
-    dend = dend_json_2_nodes_n_edges(dend_json_path)
-    dend_tree = read_dendrogram_tree(dend_json_path)
-
     path_parts = dend_json_path.split(os.path.sep)
     taxon = path_parts[len(path_parts) - 1].split(".")[0]
 
-    taxonomy_config = get_taxonomy_configuration(config, taxon)
+    taxonomy_config = read_taxonomy_config(taxon)
+
+    dend = dend_json_2_nodes_n_edges(dend_json_path)
+    dend_tree = read_dendrogram_tree(dend_json_path)
 
     if taxonomy_config:
         subtrees = get_subtrees(dend_tree, taxonomy_config)
@@ -32,18 +29,16 @@ def generate_pattern_table(dend_json_path, output_filepath):
             d = dict()
             d['defined_class'] = ALLEN_DEND_ + o['cell_set_accession']
             d['gross_cell_type'] = get_gross_cell_type(o['cell_set_accession'], subtrees, taxonomy_config)
-            d['taxon'] = taxon
+            d['taxon'] = taxonomy_config['Species'][0]
             d['brain_region'] = taxonomy_config['Brain_region'][0]
 
             if o['cell_set_accession'] in denorm_markers:
                 d['denorm_marker_list'] = denorm_markers[o['cell_set_accession']]
-            else:
-                d['denorm_marker_list'] = ''
-
-            if o['cell_set_accession'] in minimal_markers:
                 d['minimal_marker_list'] = minimal_markers[o['cell_set_accession']]
             else:
+                d['denorm_marker_list'] = ''
                 d['minimal_marker_list'] = ''
+
             dl.append(d)
 
     robot_template = pd.DataFrame.from_records(dl)
@@ -58,42 +53,10 @@ def get_gross_cell_type(_id, subtrees, taxonomy_config):
     return gross_cell_type
 
 
-def get_subtrees(dend_tree, taxonomy_config):
-    """
-    For each root node in the taxonomy creates the list of subtree nodes
-    Args:
-        dend_tree: dendrogram networkx representation
-        taxonomy_config: taxonomy configuration
-
-    Returns: list of subtree nodes
-
-    """
-    subtrees = []
-    for root_node in taxonomy_config['Root_nodes']:
-        descendants = nx.descendants(dend_tree, root_node['Node'])
-        descendants.add(root_node['Node'])
-        subtrees.append(descendants)
-    return subtrees
-
-
-def get_taxonomy_configuration(config, taxonomy):
-    """
-    Lists all taxonomies that has a configuration in the config
-    Args:
-        config: configuration file
-
-    Returns: List of taxonomy names that has a configuration
-
-    """
-    for taxonomy_config in config:
-        if taxonomy_config["Taxonomy_id"] == taxonomy:
-            return taxonomy_config
-    return
-
-
 def get_denorm_markers(taxon, ensmusg_names):
     denom_marker_path = "../templates/{}_markers_denormalized.tsv".format(taxon).replace("CCN", "CS")
     return read_markers(denom_marker_path, ensmusg_names)
+
 
 def get_minimal_markers(taxon, ensmusg_names):
     marker_path = "../markers/{}_markers.tsv".format(taxon).replace("CCN", "CS")
