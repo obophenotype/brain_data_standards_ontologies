31e6a61299d4489f05928764d7b87462c8c1babb
hkir-dev <huseyin_kir@yahoo.com>
2021-09-30 15:45:19 +0100

    human taxonomy generated  #174

--- src/scripts/nomenclature_tools.py
+++ src/scripts/nomenclature_tools.py
@@ -12,6 +12,7 @@ def nomenclature_2_nodes_n_edges(taxonomy_file_path):
     out['nodes'] = []
     out['edges'] = set()
 
+    child_cell_sets = list()
     nomenclature_records = read_csv(taxonomy_file_path, id_column=NOMENCLATURE_COLUMNS.index('cell_set_accession'))
     for node_cell_set_accession in nomenclature_records:
         columns = nomenclature_records[node_cell_set_accession]
@@ -20,7 +21,36 @@ def nomenclature_2_nodes_n_edges(taxonomy_file_path):
 
         children_str = columns[NOMENCLATURE_COLUMNS.index('child_cell_set_accessions')]
         if children_str:
-            children = children_str.strip().split('|')
-            for child in children:
-                out['edges'].add((child, node_cell_set_accession))
+            children = set(children_str.strip().split('|'))
+        else:
+            children = {node_cell_set_accession}
+        node = {"node_cell_set_accession": node_cell_set_accession, "children": children}
+        child_cell_sets.append(node)
+
+    sorted_child_cell_sets = sorted(child_cell_sets, key=lambda x: len(x["children"]))
+    for child_cell_sets in sorted_child_cell_sets:
+        parent_node = find_next_inclusive_node(sorted_child_cell_sets, child_cell_sets)
+        if parent_node:
+            print(child_cell_sets["node_cell_set_accession"] + " <-- " + parent_node["node_cell_set_accession"])
+            out['edges'].add((child_cell_sets["node_cell_set_accession"], parent_node["node_cell_set_accession"]))
+
     return out
+
+
+def find_next_inclusive_node(sorted_child_cell_sets, current_node):
+    """
+    Find the first node whose children are the minimal container of the children of the current node.
+    Args:
+        sorted_child_cell_sets: list of the nodes and their children
+        current_node: node to search its parent node.
+
+    Returns: parent node info
+    """
+    is_consecutive = False
+    for child_cell_sets in sorted_child_cell_sets:
+        if is_consecutive and current_node["children"].issubset(child_cell_sets["children"]):
+            return child_cell_sets
+        if child_cell_sets == current_node:
+            is_consecutive = True
+
+    return None
--- src/test/template_generation_utils_test.py
+++ src/test/template_generation_utils_test.py
@@ -2,10 +2,13 @@ import unittest
 import os
 import csv
 from dendrogram_tools import dend_json_2_nodes_n_edges
+from nomenclature_tools import nomenclature_2_nodes_n_edges
 from template_generation_utils import get_synonyms_from_taxonomy, get_synonym_pairs, \
     PAIR_SEPARATOR, OR_SEPARATOR, read_taxonomy_config, get_subtrees, read_dendrogram_tree
 
 PATH_DENDROGRAM_JSON = os.path.join(os.path.dirname(os.path.realpath(__file__)), "./test_data/CCN202002013.json")
+PATH_NOMENCLATURE_CSV = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                     "./test_data/nomenclature_table_CCN201912131.csv")
 
 
 class TemplateUtilsTest(unittest.TestCase):
@@ -62,6 +65,22 @@ class TemplateUtilsTest(unittest.TestCase):
         self.assertTrue("cell_set_label:RNAseq 022-025" in node50_pairs)
         self.assertTrue("cell_set_aligned_alias:''" in node50_pairs)
         self.assertTrue("cell_set_additional_aliases:''" in node50_pairs)
+
+    def test_nomenclature_nodes_edges(self):
+        tree = nomenclature_2_nodes_n_edges(PATH_NOMENCLATURE_CSV)
+        nodes = tree['nodes']
+
+        self.assertEqual(177, len(nodes))
+        self.assertEqual("CS201912131_4", nodes[3]["cell_set_accession"])
+        self.assertEqual("CS201912131_21", nodes[20]["cell_set_accession"])
+        self.assertEqual("CS201912131_51", nodes[50]["cell_set_accession"])
+        self.assertEqual("CS201912131_181", nodes[176]["cell_set_accession"])
+
+        edges = tree['edges']
+        self.assertTrue(("CS201912131_41", "CS201912131_163") in edges)
+        self.assertTrue(("CS201912131_40", "CS201912131_144") in edges)
+        self.assertTrue(("CS201912131_39", "CS201912131_160") in edges)
+        self.assertTrue(("CS201912131_38", "CS201912131_162") in edges)
 
     # not test
     def report_missing_alias(self):
