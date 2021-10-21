c9d14976b998a2e11bc750141243f25e31f1af7a
hkir-dev <huseyin_kir@yahoo.com>
2021-10-21 13:21:18 +0100

    visualisation nomenclature and multi-inheritance support

--- src/scripts/dendrogram_visualisation.py
+++ src/scripts/dendrogram_visualisation.py
@@ -5,29 +5,38 @@ import matplotlib.pyplot as plt
 from networkx.drawing.nx_agraph import graphviz_layout
 from networkx.algorithms.traversal.depth_first_search import dfs_tree
 from dendrogram_tools import dend_json_2_nodes_n_edges
-from template_generation_utils import read_dendrogram_tree, index_dendrogram
+from template_generation_utils import read_dendrogram_tree, index_dendrogram, generate_dendrogram_tree
 from marker_tools import read_marker_file
+from nomenclature_tools import nomenclature_2_nodes_n_edges
 
-NODE_LABEL_DISPLACEMENT = 800
+TAXON = "CS202002013"
+# TAXON = "CS201912132"
+
+NODE_LABEL_DISPLACEMENT = 1200
 
 NODE_Y_DISPLACEMENT = 300
 
-GAP_BETWEEN_LEAFS = 400
+GAP_BETWEEN_LEAFS = 500
 
 INTERMEDIATE_NODE_SIZE = 300
 
 LEAF_NODE_SIZE = 50
 
-PATH_DEND_JSON = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../dendrograms/CCN202002013.json")
+PATH_DEND_JSON = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../dendrograms/" + TAXON + ".json")
 
-PATH_MARKERS = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../markers/CS202002013_markers.tsv")
+PATH_NMN_TABLE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../dendrograms/nomenclature_table_CCN202002013.csv")
+
+PATH_MARKERS = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../markers/" + TAXON + "_markers.tsv")
 
 CLUSTER_ORDER = ["Lamp5", "Sncg", "Vip", "Sst", "Pvalb", "L2", "L4", "L5", "L6", "Meis", "OPC", "Astro", "Oligo",
                  "Endo", "VLMC", "SMC", "Peri", "Micro", "PVM"]
 
 
 def visualise_tree(root=None, exact_order=True):
-    tree = read_dendrogram_tree(PATH_DEND_JSON)
+    # tree = read_dendrogram_tree(PATH_DEND_JSON)
+    dend = nomenclature_2_nodes_n_edges(PATH_NMN_TABLE)
+    tree = generate_dendrogram_tree(dend)
+
     if root is not None:
         tree = dfs_tree(tree, root)
     marker_expressions = read_marker_file(PATH_MARKERS)
@@ -35,7 +44,7 @@ def visualise_tree(root=None, exact_order=True):
     add_label_metadata(tree)
     node_colors, node_labels, node_sizes, pos = decorate_nodes(marker_expressions, tree)
 
-    all_leafs = [x for x in tree.nodes(data=True) if tree.out_degree(x[0]) == 0 and tree.in_degree(x[0]) == 1]
+    all_leafs = [x for x in tree.nodes(data=True) if tree.out_degree(x[0]) == 0]
 
     position_leaf_nodes(all_leafs, exact_order, pos)
     position_intermediate_nodes(all_leafs, pos, tree)
@@ -53,7 +62,7 @@ def position_leaf_nodes(all_leaves, exact_order, pos):
     if exact_order:
         # sort by accession_id increasing
         leaves = list(all_leaves)
-        list.sort(leaves, key=lambda node: int(str(node[0]).replace("CS202002013_", "")))
+        list.sort(leaves, key=lambda node: int(str(node[0]).replace(TAXON + "_", "")))
         leaf_order = list()
         for node in leaves:
             leaf_order.append(node[1]["label"])
@@ -84,7 +93,8 @@ def position_cluster_leafs(cluster, pos, all_leafs, min_depth, last_x=2):
 
 
 def add_label_metadata(tree):
-    out = dend_json_2_nodes_n_edges(PATH_DEND_JSON)
+    # out = dend_json_2_nodes_n_edges(PATH_DEND_JSON)
+    out = nomenclature_2_nodes_n_edges(PATH_NMN_TABLE)
     dend_dict = index_dendrogram(out)
     for node in tree.nodes(data=True):
         dend_node = dend_dict[node[0]]
@@ -97,12 +107,12 @@ def decorate_nodes(marker_expressions, tree):
     color_map = []
     node_sizes = []
     for node in tree.nodes(data=True):
-        if tree.out_degree(node[0]) == 0 and tree.in_degree(node[0]) == 1:
-            node_id = str(node[0]).replace("CS202002013_", "")
+        if tree.out_degree(node[0]) == 0:
+            node_id = str(node[0]).replace(TAXON + "_", "")
             labels[node[0]] = node[1]["label"] + " (" + node_id + ")"
             node_sizes.append(LEAF_NODE_SIZE)
         else:
-            labels[node[0]] = str(node[0]).replace("CS202002013", "")
+            labels[node[0]] = str(node[0]).replace(TAXON, "")
             node_sizes.append(INTERMEDIATE_NODE_SIZE)
 
         # nodes that also exist in the marker file will be displayed as red, others as blue
@@ -112,7 +122,7 @@ def decorate_nodes(marker_expressions, tree):
         else:
             # sky blue
             color_map.append('#00BFFF')
-    plt.title('CCN202002013')
+    plt.title(TAXON)
     pos = graphviz_layout(tree, prog='dot')
     return color_map, labels, node_sizes, pos
 
